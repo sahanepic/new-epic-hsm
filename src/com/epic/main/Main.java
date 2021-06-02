4f387629a904a22fc22455af5dccdde755924577
sahanbcs <sahanepic@gmail.com>
2021-06-02 14:45:53 +0530

    properties save

--- src/com/epic/main/Main.java
+++ src/com/epic/main/Main.java
@@ -4,10 +4,7 @@ package com.epic.main;
 
 import com.epic.utill.Print;
 
-import java.io.BufferedReader;
-import java.io.Console;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 
@@ -16,15 +13,24 @@ public class Main {
 
         InputStreamReader  rr = new InputStreamReader(System.in);
         BufferedReader br =  new BufferedReader(rr);
-        Print.sysOutPrint("Enter your name: ");
-        String n=br.readLine();
+        Print.sysOutPrint("Enter your User name: ");
+        String username=br.readLine();
         Print.sysOutPrint("Enter your password: ");
-//        char[] pass = System.console().readPassword() ;
-      //  System.out.println("Welcome "+new String(pass));
-        System.out.println("danidu");
-        char[] pass = System.console().readPassword() ;
-        System.out.println("Welcome "+new String(pass));
-        System.out.println("Welcome Sahan " );
+        String password= null;
+        Console c=System.console();
+        if (c==null) { //IN  IDE
+            password = br.readLine().trim();
+            if (password.length()>0) {
+                Print.sysOutPrintln("Valid Password ");
+            } else {
+                Print.sysOutPrintln("Invalid Password ");
+            }
+        } else { //Outside Eclipse IDE
+            Print.sysOutPrint( password = new String(c.readPassword("Password: ")));
+        }
+
+        Print.sysOutPrint("Enter your Name "+username + "  and  password: " + password );
+
 
     }
 
